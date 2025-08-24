b2248322828f728b669161c0e2cb54e80f1af0ec
Bruno Oliveira <bruhensant@gmail.com>
2025-08-24 14:11:56 -0300

    feat(search): adds filter container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
 
 				<SearchBox></SearchBox>
 
-				<div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 rounded-md p-6 
+				<div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 rounded-md p-6
 					bg-mauve-alpha-3 backdrop-blur-xs">
 
 					{movieMockList.map((movie) => (
--- src/components/elements/search-box.tsx
+++ src/components/elements/search-box.tsx
@@ -1,14 +1,31 @@
+"use client"
+
 import { SearchInput } from "@/components/ui/input"
 import { Button } from "../ui/button"
 import { Icon } from "@iconify/react"
+import { useState } from "react"
+
 
 export function SearchBox(){
+	const [filterContainerState, setFilterContainerState] = useState(false)
 	return(
-		<div className="relative w-full flex items-center justify-center p-6 gap-3">
+		<div className="relative w-full flex flex-col items-center justify-center p-6 gap-4">
+			<div className="flex items-center justify-center gap-3">
+
 			<SearchInput placeholder="Pesquise por filmes" className="w-70 sm:w-150" type="search"/>
-			<Button variant={'secondary'} className="h-14">
+			<Button variant={'secondary'} className="h-14" onClick={() => setFilterContainerState(!filterContainerState)}>
 				<Icon icon="lets-icons:filter"/>
 			</Button>
+			</div>
+
+			{
+				filterContainerState ?
+				 <div className="border transition-all *:transition-all rounded-md bg-mauve-alpha-3 backdrop-blur-xs p-6">
+					filtros aqui
+
+				</div> : <div></div>
+			}
+		
 		</div>
 	)
 }
